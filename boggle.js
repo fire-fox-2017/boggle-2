314ff75299f5d7ef7261690f3f8c29dee3c1631f
oscarhermawan <franky1790@gmail.com>
2017-04-03 11:24:24 +0700

    boggle-2-done

--- boggle.js
+++ boggle.js
@@ -1,47 +1,50 @@
 class BoggleBoard{
   constructor(jumlahPapan){
     this._papan = jumlahPapan;
-    this._board = this.testPapan();   //TEST MAKE PAPAN BUATAN
-    this._kamus = ['NAX', 'NAAX', 'AKAX']
-    // this._kamus = ['AYO', 'APA',
-    //                 'BAB', 'BIA',
-    //                 'CIA', 'CAYO', 'COY',
-    //                 'DIA', 'DAI',
-    //                 'EKA', 'ELG', 'EGO', 'ERS', 'ETY',
-    //                 'FRA', 'FAR', 'FAT',
-    //                 'GILA', 'GO', 'GI', 'GIL', 'GOR', 'GIT',
-    //                 'HRY', 'HRD', 'HTM','HAI',
-    //                 'IHA', 'INA', 'IGO', 'ILK', 'ILW', 'IMN',
-    //                 'KGT', 'KMN', 'KM', 'KAN',
-    //                 'MKN', 'MNM', 'MGO', 'MIT', 'MAKAN',
-    //                 'OPO', 'ORG',
-    //                 'TANGO',
-    //                 'ZOLA', 'ZTY' ]
+    //this._board = this.testPapan()   //TEST MAKE PAPAN BUATAN
+    this._board = this.shake()
+    // this._kamus = ['MAKAX', 'ZAS']
+    this._kamus = ['AYO', 'APA',
+                    'BAB', 'BIA',
+                    'CIA', 'CAYO', 'COY',
+                    'DIA', 'DAI',
+                    'EKA', 'ELG', 'EGO', 'ERS', 'ETY',
+                    'FRA', 'FAR', 'FAT',
+                    'GILA', 'GO', 'GI', 'GIL', 'GOR', 'GIT',
+                    'HRY', 'HRD', 'HTM','HAI',
+                    'IHA', 'INA', 'IGO', 'ILK', 'ILW', 'IMN',
+                    'KGT', 'KMN', 'KM', 'KAN',
+                    'MKN', 'MNM', 'MGO', 'MIT', 'MAKAN',
+                    'NHT', 'NXT', 'NN', 'NEO', 'NGH', 'NAT', 'NOX',
+                    'OPO', 'ORG',
+                    'TANGO',
+                    'ZOLA', 'ZTY' ]
   }
 
   testPapan(){
     let arr = [];
-    arr = [['M', 'A', 'K', 'X'],
-           ['N', 'A', 'A', 'X'],
-           ['T', 'X', 'X', 'O'],
-           ['U', 'X', 'G', 'L']]
+    arr = [['M', 'A', 'K', 'O'],
+           ['A', 'Z', 'A', 'G'],
+           ['O', 'A', 'X', 'S'],
+           ['A', 'X', 'S', 'X']]
     return arr;
   }
 
   shake(){
-    let board = [];
+    let arr = [];
     let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     for(let i=0; i<this._papan; i++){
-      this._board.push([]);
+      arr.push([]);
       for(let j=0;j<this._papan;j++){
-        this._board[i][j] = possible.charAt(Math.floor(Math.random() * possible.length));
+        arr[i][j] = possible.charAt(Math.floor(Math.random() * possible.length));
       }
     }
-    console.log(this._board)
+    return arr;
   }
 
   solve(){
     let arr = []
+    let kata
     for(let i=0;i<this._kamus.length;i++){
       let tmp = this._kamus[i];
       for(let row=0; row<this._papan; row++){
@@ -51,17 +54,32 @@ class BoggleBoard{
           //CEK HURUF PERTAMA tmp[0] DI CORDINAT BERAPA AJA DAPATNYA
           if(this._board[row][col] == tmp[0]){
             // this.cekWord(tmp.slice(1), [row,col], [[row, col]], tmp[0] )
-            arr.push(this.cekWord(tmp.slice(1), [row,col], [[row, col]], tmp[0] ))
-
+            // console.log('TEST BACAAN '+i+ '----'+this.cekWord(tmp.slice(1), [row,col], [[row, col]], tmp[0] ))
+            let kata = this.cekWord(tmp.slice(1), [row,col], [[row, col]], tmp[0], tmp)
+            if(kata === tmp){
+              arr.push(kata)
+            }
           }
         }
       }
-      console.log(arr)
     }
+    // console.log(arr)
+
+    if(arr.length == 0){
+      console.log('tidak ada data ditemukan')
+    }
+    else{
+      console.log(`${arr.length} words found :`)
+      for(let j=0;j<arr.length;j++){
+        console.log(arr[j])
+      }
+    }
+
   }
 
 
-  cekWord(kata, posSaatIni, posPernahDiinjek, tmpKata){
+  cekWord(kata, posSaatIni, posPernahDiinjek, tmpKata, kataAsli){
+    let finish;
     if(kata.length == 0){
       return tmpKata
     }
@@ -79,11 +97,20 @@ class BoggleBoard{
                 // console.log(posPernahDiinjek)
                 // console.log('Kordinat tujuan :'+[posSaatIni[0]+area[i][0], posSaatIni[1]+area[i][1]]+ ', Pernah Diinjek : '+posPernahDiinjek+'---'+this.pernahDiinjek([posSaatIni[0]+area[i][0], posSaatIni[1]+area[i][1]], posPernahDiinjek))
                 posPernahDiinjek.push([newrow,newcol])
-                var m = this.cekWord(kata.slice(1), [newrow,newcol], posPernahDiinjek, tmpKata+kata[0])
+
+
+                let finish = this.cekWord(kata.slice(1), [newrow,newcol], posPernahDiinjek, tmpKata+kata[0], kataAsli)
+                if(finish == kataAsli){
+                  return finish;
+                }
+                else {
+                  this.cekWord(kata.slice(1), [newrow,newcol], posPernahDiinjek, tmpKata+kata[0], kataAsli)
+                }
+
               }
             }
         }
-    return m;
+    return ''
 
 
 
@@ -98,12 +125,6 @@ class BoggleBoard{
         // console.log('false')
         return false
       }
-
-      // else {
-      //   console.log(posTujuan[0], posPernahDiinjek[i][0], posTujuan[1], posPernahDiinjek[i][1])
-      //   console.log('true')
-      //   return true
-      // }
     }
     return true
   }
