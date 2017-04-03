d68f51d8e855c2c1f89bbc6d4aa1ddc6d440fc82
haryanawisnu <arya.wisnu05@gmail.com>
2017-04-03 08:50:44 +0700

    done revisi2

--- boggle.js
+++ boggle.js
@@ -2,7 +2,7 @@
 
 class BoggleBoard {
   constructor() {
-    this.arrAlfabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","e","s","t","u","v","w","x","y","z"];
+    this.arrAlfabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","e","s","t","u","v","w","x","y","z","a","i","u","e","o"];
     this.board=[];
     this.arrData=[];
     this.arrResult=[];
